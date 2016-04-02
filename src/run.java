11fd6da0a1c367502bd9fca5d054b6be57fbd665
David Hunt <huntdog1541@gmail.com>
2016-04-01 21:46:07 -0500

    Push

--- src/run.java
+++ src/run.java
@@ -1,6 +1,35 @@
+import java.io.BufferedReader;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
+
 public class run
 {
 	public static void main(String[] args) {
 		System.out.println("Hello World!");
+		execute("test.txt");
+	}
+
+	public static void execute(String fileName)
+	{
+		String line = null;
+
+		try{
+			FileReader fileReader = new FileReader(fileName);
+			BufferedReader buffered = new BufferedReader(fileReader);
+			while((line = buffered.readLine()) != null){
+				System.out.println(line);
+			}
+			buffered.close();
+		}
+		catch(FileNotFoundException ex)
+		{
+			ex.printStackTrace();
+		}
+		catch(IOException ex)
+		{
+			ex.printStackTrace();
+		}
 	}
 }
+
